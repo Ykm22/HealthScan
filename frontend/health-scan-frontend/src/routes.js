c2847e10225f12d3ff4a248808d67bc07fef272f
Kosmaan <radu.cosmin.30.1@gmail.com>
2024-09-01 11:45:39 +0300

    nothing

--- frontend/health-scan-frontend/src/routes.js
+++ frontend/health-scan-frontend/src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home'; // Adjust the import path if necessary
+import Home from './pages/Home/Home';
 
 function App() {
   return (
