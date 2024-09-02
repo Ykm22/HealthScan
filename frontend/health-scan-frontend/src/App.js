6c13f200b4d82c00324b5aba9becf01e976584cb
Kosmaan <radu.cosmin.30.1@gmail.com>
2024-09-02 11:33:34 +0300

    added about us without header + footer

--- frontend/health-scan-frontend/src/App.js
+++ frontend/health-scan-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import AboutUs from './pages/About us/AboutUs';
 
 function App() {
   return (
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
         </Routes>
       </Router>
   );
